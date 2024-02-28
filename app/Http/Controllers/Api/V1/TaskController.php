835d8373a3473992bc61ba4614ee1283fd200584
karine <karine.porto@gmail.com>
2024-02-28 17:04:24 +0000

    ativando o sanctum

--- app/Http/Controllers/Api/V1/TaskController.php
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -49,6 +49,8 @@ class TaskController extends Controller
      */
     public function destroy(Task $task)
     {
-        //
+        $task->delete();
+        return response()->noContent();
+
     }
 }
