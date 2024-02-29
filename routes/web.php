257082379b4119de79466fdd40f568b135568242
karine <karine.porto@gmail.com>
2024-02-29 11:10:38 +0000

    register

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,4 +25,5 @@ Route::prefix('auth')->group(function(){
 
     Route::post('/login', LoginController::class);
     Route::post('/logout', LogoutController::class);
+    Route::post('/register', RegisterController::class);
 });
