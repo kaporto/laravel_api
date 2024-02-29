93df8de7b4ac60b3d73a959376760168c4fdabd8
karine <karine.porto@gmail.com>
2024-02-29 14:53:15 +0000

    linking users to other resources

--- routes/api.php
+++ routes/api.php
@@ -18,10 +18,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::middleware('auth:sanctum')->prefix('v1')->group(function(){
-    Route::apiResource('/tasks',TaskController::class);
-    Route::patch('/tasks/{task}/complete', CompleteTaskController::class);
-});
+
+require __DIR__ .'/api/v1.php';
+require __DIR__ .'/api/v2.php';
+
 
 Route::prefix('auth')->group(function(){
 
